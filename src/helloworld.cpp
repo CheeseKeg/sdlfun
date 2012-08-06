611b658854f59a75bd2bab951c3d09504e28a0fb
Brandon DeRosier <cheesekeg@gmail.com>
2012-08-06 08:12:40 -0400

    Completed first tutorial

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,53 +1,30 @@
-#include <iostream>
 #include <SDL/SDL.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
 
-void init()
+int main (int argc, char** argv)
 {
-	glClearColor(0.0,0.0,0.0,1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45.0,640.0/480.0,1.0,500.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-}
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Surface* screen;
+  screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
 
-void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-		glVertex3f(0.0,2.0,-5.0);
-		glVertex3f(-2.0,-2.0,-5.0);
-		glVertex3f(2.0,-2.0,-5.0);
-	glEnd();
-}
+  bool running = true;
 
-int main()
-{
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Surface* screen=SDL_SetVideoMode(640,480,32,SDL_SWSURFACE|SDL_OPENGL);
-	bool running=true;
-	Uint32 start;
-	SDL_Event event;
-	init();
-	while(running)
-	{
-		start=SDL_GetTicks();
-		while(SDL_PollEvent(&event))
-		{
-			switch(event.type)
-			{
-				case SDL_QUIT:
-					running=false;
-					break;
-			}
-		}
-		display();
-		SDL_GL_SwapBuffers();
-		if(1000/30>(SDL_GetTicks()-start))
-			SDL_Delay(1000/30-(SDL_GetTicks()-start));
-	}
-	SDL_Quit();
-	return 0;
+  while (running)
+  {
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+      case SDL_QUIT:
+	running = false;
+	break;
+      }
+    }
+    // Login & render
+
+    SDL_Flip(screen);
+  }
+
+  SDL_Quit();
+  return 0;
 }
