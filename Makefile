2fa89aeb83fa9d048cdb5811d3cb69a9813e4102
Brandon DeRosier <cheesekeg@gmail.com>
2012-08-09 20:35:48 -0400

    Completed tenth OpenGL tutorial.

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ bin = bin/
 src = src/
 cc = g++
 
-programs = helloworld regulatefps rectangle keyhandling loadimage colorkey blitimage animation ttf opengl-intro opengl-basiccolors opengl-transformations opengl-displaylist opengl-cube opengl-lighting opengl-textures opengl-objloader opengl-fog
+programs = helloworld regulatefps rectangle keyhandling loadimage colorkey blitimage animation ttf opengl-intro opengl-basiccolors opengl-transformations opengl-displaylist opengl-cube opengl-lighting opengl-textures opengl-objloader opengl-fog opengl-matrix
 #binaries = $(addprefix $(bin), $(programs))
 #progexec = $(addprefix exec., $(programs))
 
@@ -147,6 +147,11 @@ $(bin)$(t17) : $(bin)$(t17).o
 t18 = opengl-fog
 $(t18) : $(bin)$(t18)
 $(bin)$(t18) : $(bin)$(t18).o $(bin)functions.o
+	$(clink) $(glflags) $^
+
+t19 = opengl-matrix
+$(t19) : $(bin)$(t19)
+$(bin)$(t19) : $(bin)$(t19).o $(bin)functions.o
 	$(clink) $(glflags) $^
 
 $(bin)%.o : $(src)%.cpp
---
+++ src/opengl-matrix.cpp
@@ -0,0 +1,96 @@
+#include "functions.h"
+
+float angle = 0.0;
+
+int cube;
+
+void init(SDL_Surface* screen)
+{
+  glClearColor(0.5, 0.5, 0.5, 1.0);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(45, static_cast<float>(screen->w)/static_cast<float>(screen->h), 1.0, 500.0);
+  glMatrixMode(GL_MODELVIEW);
+  glShadeModel(GL_SMOOTH); // This is the default setting anyways.
+
+  glEnable(GL_DEPTH_TEST);
+
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  float lightcolor[] = {1.0, 1.0, 1.0, 1.0};
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, lightcolor);
+
+  cube = loadObject("model/obj/monkeymed.obj");
+}
+
+void display()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glLoadIdentity();
+
+  float lightposition[] = {-1.0, 1.0, -2.0, 1.0};
+  glLightfv(GL_LIGHT0, GL_POSITION, lightposition);
+
+  glPushMatrix();
+    glTranslatef(0.0, 0.0, -10.0);
+    glRotatef(angle, 1.0, 1.0, 1.0);
+
+    glCallList(cube);
+  glPopMatrix();
+
+  glTranslatef(0.0, 0.0, -10.0);
+  glRotatef(-angle, 1.0, 1.0, 1.0);
+  glTranslatef(3.0, 0.0, -2.0);
+  glCallList(cube);
+}
+
+int main (int argc, char** argv)
+{
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  SDL_Surface* screen;
+  screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE | SDL_OPENGL);
+  init(screen);
+
+  bool running = true;
+  Uint32 start;
+
+  unsigned const int FPS = 30;
+
+  SDL_Event event;
+  while (running)
+  {
+    start = SDL_GetTicks();
+
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+      case SDL_QUIT:
+	running = false;
+	break;
+      default:
+	break;
+      }
+    }
+
+    display();
+    SDL_GL_SwapBuffers();
+
+    angle += 0.5;
+    if (angle >= 360)
+    {
+      angle -= 360;
+    }
+
+    if (SDL_GetTicks() - start < 1000/FPS)
+    {
+      SDL_Delay(1000/FPS - (SDL_GetTicks()-start));
+    }
+  }
+
+  SDL_Quit();
+  return 0;
+}
